de0920ea4620ccaa6e46aa79fab674f59e4199b4
Sam Mallamaci <sam.mallamaci@gmail.com>
2016-09-18 03:59:52 -0400

    fuck

--- conversatio_app/views.py
+++ conversatio_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello my dudes")
+    return render(request, '/conversatio_app/index.html')
 # Create your views here.
