d2d7a8dfe41de203c2fb1e950075e66a3b3b3d40
Cole Hoffbauer <colehoff7@gmail.com>
2016-09-18 00:32:51 -0400

    first real commit, python client with working watson calls speech->text in 10 second increments

---
+++ conversatio.py
@@ -0,0 +1,17 @@
+import json
+import recording
+from os.path import join, dirname
+from watson_developer_cloud import SpeechToTextV1
+
+if __name__ == "__main__":
+   speech_to_text = SpeechToTextV1(
+       username='e28da131-47ff-49b2-bb21-9d78290cd945',
+       password='noFUouvEAxKn',
+       x_watson_learning_opt_out=False
+   )  
+   
+   recording.init();
+
+   with open(join(dirname(__file__), 'demo.wav'), 'rb') as audio_file:
+       print(json.dumps(speech_to_text.recognize(
+           audio_file, content_type='audio/wav', continuous = True, timestamps=True), indent=2))
